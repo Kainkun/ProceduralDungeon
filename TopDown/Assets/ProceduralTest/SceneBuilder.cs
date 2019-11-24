f181e0958f32a7589dc56c48d80f9ef200975214
Kainkun <kainkun100@gmail.com>
2019-11-23 21:20:57 -0800

    tilemap work

---
+++ TopDown/Assets/ProceduralTest/SceneBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SceneBuilder : MonoBehaviour
+{
+    [SerializeField]
+    int width, height;
+
+    int[,] terrainMap;
+
+    [SerializeField]
+    Tilemap groundMap, colliderMap;
+    [SerializeField]
+    Tile[] groundTiles, colliderTiles;
+
+    void Start()
+    {
+        buildScene();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void buildScene()
+    {
+        clearMap();
+        terrainMap = new int[width, height];
+        initPos();
+        setTiles();
+
+
+    }
+
+    void setTiles()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < height; x++)
+            {
+                groundMap.SetTile(new Vector3Int(x, y, 0), groundTiles[0]);
+            }
+        }
+    }
+
+    void initPos()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < height; x++)
+            {
+                terrainMap[x, y] = Random.Range(0, 2);
+            }
+        }
+    }
+
+    void clearMap()
+    {
+        groundMap.ClearAllTiles();
+        colliderMap.ClearAllTiles();
+
+        terrainMap = null;
+    }
+}
